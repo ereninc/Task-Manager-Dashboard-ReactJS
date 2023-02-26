ff2a0673f65829017de09df031bc6b0631b06f3c
Eren <erencan_inanci@hotmail.com>
2023-02-27 00:33:23 +0300

    router refactor

--- src/index.js
+++ src/index.js
@@ -5,30 +5,12 @@ import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/global.css";
 
-import HomePage from "./pages/home-page/";
-import MainLayout from "./layouts/main-layout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider
-      router={createBrowserRouter([
-        {
-          path: "/",
-          element: <MainLayout />,
-          children: [
-            {
-              path: "/",
-              element: <HomePage />,
-            },
-            // {
-            //   path: "/test",
-            //   element: <h1>TEST</h1>,
-            // },
-          ],
-        },
-      ])}
-    ></RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
---
+++ src/router/index.jsx
@@ -0,0 +1,22 @@
+import HomePage from "../pages/home-page/";
+import MainLayout from "../layouts/main-layout";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      // {
+      //   path: "/test",
+      //   element: <h1>TEST</h1>,
+      // },
+    ],
+  },
+]);
+
+export { router };
